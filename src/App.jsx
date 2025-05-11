4da9343af61ac628603248ee7cd8456b477b6944
CodeB1ack <patrickcledford@gmail.com>
2025-05-11 09:45:01 -0400

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import ToDoList from "./To-do-list.jsx";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar.jsx";
+
+
+
 
 function App() {
 
-  return (<ToDoList />)
-
+  return (<ToDoList />,
+     <BrowserRouter>
+       <Navbar />
+       <Routes>
+         <Route path="/" element={<ToDoList />} />
+         <Route path="/contacts" element={<h1>Contacts</h1>} />
+       </Routes>
+     </BrowserRouter>
+  );
+ 
 }
 
 export default App
