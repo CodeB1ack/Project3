8fe6ffdf56c65c27cf254fcf7845295f96a315eb
CodeB1ack <patrickcledford@gmail.com>
2025-05-11 10:50:38 -0400

    setup contact form

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,8 +1,30 @@
 import React from "react";
+import "./Contact.css"
 
-function Contact() {
+const Contact = () => {
     return (
-            <h1>Contact Page</h1>
+            <section className="contact">
+                <form>
+                    <h2>Contact Form</h2>
+                    <div className="input-box">
+                        <label>First Name</label>
+                        <input type="text" className="field" placeholder="Enter your name" required />
+                    </div>
+                    <div className="input-box">
+                        <label>Last Name</label>
+                        <input type="text" className="field" placeholder="Enter your last name" required />
+                    </div>
+                    <div className="input-box">
+                        <label>Email Address</label>
+                        <input type="email" className="field" placeholder="Enter your email" required />
+                    </div>
+                    <div className="input-box">
+                        <label>Your Message</label>
+                        <textarea name="" id="" className="field mess" placeholder="Enter your message" required></textarea>
+                    </div>
+                    <button type="submit">Send Message</button>
+                </form>
+            </section>
             
         
     );
